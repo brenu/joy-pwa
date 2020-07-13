b14d0b31ed7b89164c82952d63d83367615f74c7
Nicholas Rodrigues dos Santos <nicholasybiologia@gmail.com>
2020-07-13 08:59:45 -0300

    v.0.1.2
    change joystick for a primal movement

--- src/components/Control/JoyStick/index.js
+++ src/components/Control/JoyStick/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './styles.css';
 import { Joystick } from 'react-joystick-component';
 
+function handleMove() {
+    console.log('moving')
+}
+
+function handleStop() {
+    console.log('stopped');
+}
+
 const JoyStick = () => (
     <div>
-        <Joystick size={100} baseColor="red" stickColor="blue" disabled={true}></Joystick>
+        <Joystick size={100} baseColor="red" stickColor="blue" move={handleMove} stop={handleStop}></Joystick>
     </div>
 );
 
